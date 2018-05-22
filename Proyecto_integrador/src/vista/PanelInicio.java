f33e32aee50ae06a4955165d0463836757695d43
javier <javiermiok@gmail.com>
2018-05-22 19:59:52 +0200

    Cambios en PanelInicio

--- Proyecto_integrador/src/vista/PanelInicio.java
+++ Proyecto_integrador/src/vista/PanelInicio.java
@@ -8,19 +8,16 @@ public class PanelInicio extends JPanel {
 	private JLabel lblLogo;
 
 	public PanelInicio() {
-		super();
+		inicializar();
+	}
+
+	private void inicializar() {
 		setLayout(null);
 		
 		lblLogo = new JLabel("");
 		lblLogo.setIcon(new ImageIcon(PanelInicio.class.getResource("/images/logo.png")));
 		lblLogo.setBounds(43, 53, 560, 360);
 		add(lblLogo);
-		inicializar();
-	}
-
-	private void inicializar() {
-		// TODO Auto-generated method stub
-		
 	}
 	
 	public void hacerVisible() {
